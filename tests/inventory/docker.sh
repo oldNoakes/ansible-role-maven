0ed3c8a75d44ab737cd64b22498c81f9fb4153e0
noakes <old.noakes@gmail.com>
2018-01-09 16:32:30 +1100

    Fixing name to match repo name - can look into removing this dependency in the future

--- tests/inventory/docker.sh
+++ tests/inventory/docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DOCKER_HOST=127.0.0.1
-DOCKER_PORT=`docker port maven 22 | awk -F: '{ print $2 }'`
+DOCKER_PORT=`docker port ansible-role-maven 22 | awk -F: '{ print $2 }'`
 
 echo "{\"all\":{\"hosts\":[\"${DOCKER_HOST}\"],\"vars\":{\"ansible_ssh_port\":\"${DOCKER_PORT}\"}}}"
